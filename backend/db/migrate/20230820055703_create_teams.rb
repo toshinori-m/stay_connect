12ca95eac5647519bfcb0a1f53a300ff8e3e70e0
toshinori-m <toshi3587@gmail.com>
2023-08-20 19:23:21 +0900

    teamsテーブル内を修正

--- backend/db/migrate/20230820055703_create_teams.rb
+++ backend/db/migrate/20230820055703_create_teams.rb
@@ -1,6 +1,12 @@
 class CreateTeams < ActiveRecord::Migration[7.0]
   def change
     create_table :teams do |t|
+      ## Required
+      t.string :name, :null => false, :default => ""
+      t.string :area, :null => false, :default => ""
+      t.integer :sex, :null => false, :default => ""
+      t.string :track_record, :null => false, :default => ""
+
       t.references :user
       t.references :sports_type
       t.string :name
