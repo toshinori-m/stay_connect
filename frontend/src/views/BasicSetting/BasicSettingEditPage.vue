574260c00b624e36ae75efbcf205f9035bfb7208
toshinori-m <toshi3587@gmail.com>
2023-10-27 17:49:48 +0900

    BasicSettingEditPage修正

--- frontend/src/views/BasicSetting/BasicSettingEditPage.vue
+++ frontend/src/views/BasicSetting/BasicSettingEditPage.vue
@@ -2,16 +2,16 @@
   <div class="flex items-center justify-center mt-32 xl:mt-20">
     <div class="xl:w-2/5 rounded-md shadow-gray-200 bg-sky-100">
       <h2 class="text-center pt-10 font-bold text-3xl text-blue-600">基本設定編集</h2>
-      <button class="cancel_button mx-5 float-right" @click="EventSettingEditCancel">戻る</button>
+      <button class="cancel_button mx-5 float-right" @click="BasicSettingEditCancel">戻る</button>
       <div class="my-10">
         <div class="error text-sm text-red-400" v-for="(errMsg, index) in errors" :key="index">{{ errMsg }}</div>
-        <form class= "text-center" @submit.prevent="editEventSetting(users.id)">
+        <form class= "text-center" @submit.prevent="basicSettingEdit(user.id)">
           <ul>
             <li class="xl:grid grid-cols-3 gap-4">
               <label class="xl:place-self-start xl:mt-3 xl:mr-4 xl:ml-3" for="name">name</label>
-              <input class="xl:place-self-center mx-6 py-1 px-1 my-2 w-72 border-2 border-gray-200 box-border" id="name" type="text" required placeholder="イベント名" v-model="users.name">
+              <input class="xl:place-self-center mx-6 py-1 px-1 my-2 w-72 border-2 border-gray-200 box-border" id="name" type="text" required placeholder="名前" v-model="user.name">
             </li>
-            <div class="error text-sm text-red-400" v-for="(errMsg, index) in errors" :key="index">{{ errMsg }}</div>
+            <div class="error text-sm text-red-400" v-for="(errMsg, index) in error" :key="index">{{ errMsg }}</div>
             <button class="update_button mx-5">更新</button>
           </ul>
         </form>
@@ -24,15 +24,14 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      users: {},
-      errors: [],
-      backend_errors: ""
+      user: {},
+      error: null
     }
   },
   methods: {
     async getBasicSettingEdit() {
       try {
-        this.errors = []
+        this.error = null
         const res = await axios.get('http://localhost:3001/auth/users', {
           headers: {
           uid: window.localStorage.getItem('uid'),
@@ -40,13 +39,12 @@ export default {
           client: window.localStorage.getItem('client')
           }
         })
-        console.log({ res })
-        this.users = res.data.data
+        this.user = res.data.data
       } catch (error) {
-        this.errors.push('基本設定を表示出来ませんでした。')
+        this.error.push(data.errors)
       }
     },
-    EventSettingEditCancel() {
+    BasicSettingEditCancel() {
       this.$router.push({name: 'HomePage'})
     }
   },
