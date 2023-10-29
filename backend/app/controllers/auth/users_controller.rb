e6fb55d205d729ef9878ffafd035009df6ba03ff
toshinori-m <toshi3587@gmail.com>
2023-10-29 13:54:24 +0900

    BasicSettingEditPage修正

--- backend/app/controllers/auth/users_controller.rb
+++ backend/app/controllers/auth/users_controller.rb
@@ -3,19 +3,19 @@ class Auth::UsersController < ApplicationController
   
   def update
     user = User.find(params[:id])
-    return render json: { message: '成功しました', data: user }, status: 200 if user.update(user_params)
+    return render json: { data: user }, status: 200 if user.update(user_params)
 
-    render json: { message: '保存出来ませんでした', errors: user.errors }, status: 400
+    render json: {}, status: 400
   end
 
   def index
     return render json: { is_login: true, data: current_user }, status: 200 if current_user
 
-    render json: { is_login: false, message: "ユーザーが存在しません" }, status: 400
+    render json: { is_login: false }, status: 400
   end
 
   def show
-    render json: { message: '成功しました', data: User.select(:name, :image, :birthday, :sex) }, status: 200
+    render json: { data: User.select(:name, :image, :birthday, :sex) }, status: 200
   end
 
   private
