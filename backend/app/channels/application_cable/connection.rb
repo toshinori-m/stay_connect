38d9eafcb7fe9aad75c538ec487a06e08994fe8b
toshinori-m <toshi3587@gmail.com>
2023-11-14 19:09:20 +0900

    room_channel作成

--- backend/app/channels/application_cable/connection.rb
+++ backend/app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      user = User.find_by(uid: request.params[:uid])
+      if user && user.valid_token?(request.params[:'access-token'], request.params[:client])
+        user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
