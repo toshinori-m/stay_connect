0c593866400733247b5792b68961f312978685ed
toshinori-m <toshi3587@gmail.com>
2023-11-16 17:47:42 +0900

    room_channel修正

--- backend/app/channels/room_channel.rb
+++ backend/app/channels/room_channel.rb
@@ -3,15 +3,4 @@ class RoomChannel < ApplicationCable::Channel
     chat_room = ChatRoom.find(params[:room_id])
     stream_for chat_room
   end
-
-  def receive(data)
-    chat_room = ChatRoom.find(data['room_id'])
-    if message = ChatMessage.create(message: data['message'], user: current_user, chat_room: chat_room)
-      RoomChannel.broadcast_to(chat_room, {
-        message: message.message,
-        name: current_user.name,
-        created_at: message.created_at.strftime("%Y-%m-%d %H:%M:%S")
-      })
-    end
-  end
 end
