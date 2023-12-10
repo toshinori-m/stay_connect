0d90d3080f08b488f37e75e10945097e02d63a78
toshinori-m <toshi3587@gmail.com>
2023-12-10 10:12:51 +0900

    vuex導入修正

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,19 +4,6 @@ import router from './router'
 import store from './store'
 import './index.css'
 import './assets/main.css'
-import { auth } from '@/plugins/firebase'
-
-auth.onAuthStateChanged(user => {
-  if (user) {
-    store.commit('setUser', {
-      name: user.displayName,
-      email: user.email,
-      uid: user.uid
-    })
-  } else {
-    store.commit('setUser', null);
-  }
-})
 
 const app = createApp(App).use(router).use(store)
 const storedUser = localStorage.getItem('currentUser')
