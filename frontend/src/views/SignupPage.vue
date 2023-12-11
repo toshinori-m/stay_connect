164734d8f04e8e7b39626d582ddec6a320436e5d
toshinori-m <toshi3587@gmail.com>
2023-12-11 18:49:07 +0900

    Firebaseのemail login機能

--- frontend/src/views/SignupPage.vue
+++ frontend/src/views/SignupPage.vue
@@ -4,34 +4,17 @@
       <h2 class="text-center pt-10 font-bold text-3xl text-blue-600">ユーザー登録</h2>
       <div class="text-center my-5 text-blue-600">
         <form @submit.prevent="redirectToRegister">
-          <button class="md:w-3/4 w-64 my-2 text-blue-600 border-4 border-blue-300 border-double px-3 py-2">
+          <button class="md:w-3/4 w-64 my-10 text-blue-600 border-4 border-blue-300 border-double px-3 py-2">
             <span className="i-lucide-mail w-5 h-5 float-left"></span>
             <i class="px-3">メールアドレスで登録</i>
           </button>
         </form>
-
         <button @click="signInWithGoogle" class="md:w-3/4 w-64 my-2 text-blue-600 border-4 border-blue-300 border-double px-3 py-2">
           <span className="i-tabler-brand-google w-5 h-5 float-left"></span>
           <i class="px-3">Googleアカウントで登録</i>
         </button>
-
-        <button class="md:w-3/4 w-64 my-2 text-blue-600 border-4 border-blue-300 border-double px-3 py-2">
-          <span className="i-tabler-brand-twitter w-5 h-5 float-left"></span>
-          <i class="px-3">Twitterで登録</i>
-        </button>
-
-        <button class="md:w-3/4 w-64 my-2 text-blue-600 border-4 border-blue-300 border-double px-3 py-2">
-          <span className="i-tabler-brand-facebook w-5 h-5 float-left"></span>
-          <i class="px-3">Facebookで登録</i>
-        </button>
-
-        <button class="md:w-3/4 w-64 my-2 text-blue-600 border-4 border-blue-300 border-double px-3 py-2">
-          <span className="i-tabler-brand-apple w-5 h-5 float-left"></span>
-          <i class="px-3">Apple IDでサインイン</i>
-        </button>
-
         <form @submit.prevent="redirectToLogin">
-          <button class="md:w-3/4 w-70 my-5 text-blue-600 bg-clip-padding p-1 border-4 border-violet-300 border-dashed">アカウントをお持ちの方はこちら</button>
+          <button class="md:w-3/4 w-70 my-10 text-blue-600 bg-clip-padding p-1 border-4 border-violet-300 border-dashed">アカウントをお持ちの方はこちら</button>
         </form>
         <div class="error">{{ error }}</div>
       </div>
