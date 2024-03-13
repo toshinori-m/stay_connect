6d1ac2412ca742d3d17cf18edc0d6706654d0140
toshinori-m <toshi3587@gmail.com>
2024-03-13 17:45:27 +0900

    cors.rbの改善

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://issues-131.d2pcdup5dl1iel.amplifyapp.com"
+    origins ENV['CORS_ORIGINS']
 
     resource "*",
       headers: :any,
