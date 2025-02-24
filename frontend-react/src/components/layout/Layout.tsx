63ba3ba9047c44fe18d255e6545d13f5225b8c26
toshinori-m <toshi3587@gmail.com>
2025-02-24 10:23:36 +0900

    不要なファイルと未使用のコメントを整理・削除

--- frontend-react/src/components/layout/Layout.tsx
+++ frontend-react/src/components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { useAuth } from "@/contexts/AuthContext"
 import OpenHeader from "@/components/layout/OpenHeader"
-// import HomeHeader from "../components/layout/HomeHeader"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { user } = useAuth()
@@ -9,7 +8,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div>
       {/* 今は `user` が常に `null` なので、 `OpenHeader` を表示 */}
       <OpenHeader />
-      {/* {user ? <HomeHeader /> : <OpenHeader />} */}
       {children}
     </div>
   );
--- frontend-react/src/router/index.tsx
+++ frontend-react/src/router/index.tsx
@@ -1,14 +1,10 @@
 import { Routes, Route } from "react-router-dom"
 import OpenPage from "@/pages/OpenPage"
-// import SignupPage from "../pages/SignupPage"
-// import LoginPage from "../pages/LoginPage"
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<OpenPage />} />
-      {/* <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} /> */}
     </Routes>
   );
 }
